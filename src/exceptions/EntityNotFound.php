15bb3be3a824666cd69b9f9fa76ff7a2a458a7bc
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-09-07 00:34:27 +0200

    Create EntityNotFound.php

---
+++ src/exceptions/EntityNotFound.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Bermuda\Cycle;
+
+class EntityNotFound extends \RuntimeException 
+{
+    public function __construct(private string $role, private string $id)
+    {
+        parent::__construct(sprintf('Entity: %s with id: %s not found!', $role, $id), 404)
+    }
+  
+    public function getRole(): string
+    {
+        return $this->role;
+    }
+  
+    public function getId(): string
+    {
+        return $this->id;
+    }
+}
