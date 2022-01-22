c940909c353395117466bb1052755ef09ef68c82
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-01-22 22:24:23 +0200

    Update ConfigProvider.php

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -7,10 +7,6 @@ use Cycle\ORM\PromiseFactoryInterface;
 use Cycle\ORM\SchemaInterface;
 use Spiral\Database\DatabaseManager;
 
-/**
- * Class CycleProvider
- * @package Bermuda\Cycle
- */
 class ConfigProvider extends \Bermuda\Config\ConfigProvider
 {
     protected function getFactories(): array
