36723ce021d36da9e9766f26a77fdf963c69384e
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-01-23 22:23:11 +0200

    Update SchemaFactory.php

--- src/SchemaFactory.php
+++ src/SchemaFactory.php
@@ -2,21 +2,11 @@
 
 namespace Bermuda\Cycle;
 
-
 use Cycle\ORM\Schema;
 use Psr\Container\ContainerInterface;
 
-
-/**
- * Class SchemaFactory
- * @package Bermuda\Cycle
- */
 final class SchemaFactory
 {
-    /**
-     * @param ContainerInterface $container
-     * @return Schema
-     */
     public function __invoke(ContainerInterface $container): Schema
     {
         return new Schema($container->get('config')['cycle'][1] ?? []);
