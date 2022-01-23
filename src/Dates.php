5c7b1615ae35c9727ac47334f533ea8cad6d1b16
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-01-23 14:26:44 +0200

    Update Dates.php

--- src/Dates.php
+++ src/Dates.php
@@ -4,52 +4,48 @@ namespace Bermuda\Cycle;
 
 use Cycle\ORM\Select;
 use Bermuda\Utils\Date;
-use Bermuda\String\Str;
+use Bermuda\Clock\Clock;
+use Carbon\CarbonInterface;
 use Spiral\Database\Injection\Fragment;
 use Spiral\Database\Injection\Parameter;
 
 trait Dates
 {
-    protected string $dateTimeFormat = 'Y-m-d';
+    protected string $dateTimeFormat = 'Y-m-d H:i:s';
     protected string $dateTimeColumn = 'created_at';
     protected string $dateTimeStringDelimiter = ',';
     protected string $dateTimeStringRangeDelimiter = ':';
-    
-    /**
-     * @param Select $select
-     * @param \DateTimeInterface[] $dates
-     * @return Select
-     */
+
     protected function dates(Select $select, array $dates): Select
     {
         if (($count = count($dates)) == 2) {
-            return $select->where(new Fragment('Date('.$this->dateTimeColumn.')'), 'between', $dates[0]->format($this->dateTimeFormat), $dates[1]->format($this->dateTimeFormat));
+            return $select->where(new Fragment('Date('.$this->dateTimeColumn.')'), 'between', $dates[0]->toDateTimeString(), $dates[1]->toDateTimeString());
         } elseif ($count == 1) {
-            return $select->where(new Fragment('Date('.$this->dateTimeColumn.')'), $dates[0]->format($this->dateTimeFormat));
+            return $select->where(new Fragment('Date('.$this->dateTimeColumn.')'), $dates[0]->toDateTimeString());
         } elseif ($count > 2) {
             foreach ($dates as $date) {
                 $params[] = $date->format($this->dateTimeFormat);
             }
-            
+
             return $select->where(new Fragment('Date('.$this->dateTimeColumn.')'), 'in', new Parameter($params ?? []));
         }
 
         return $select;
     }
-    
+
     /**
      * @param string $dates
-     * @return \DateTimeInterface[]
+     * @return CarbonInterface[]
      */
     protected function parseDatesString(string $dates): array
     {
-        if (Str::contains($dates, $this->dateTimeStringRangeDelimiter)) {
+        if (str_contains($dates, $this->dateTimeStringRangeDelimiter)) {
             foreach (explode($this->dateTimeStringRangeDelimiter, $dates, 2) as $date) {
-                if (!Date::isDate($date)) {
+                if (!Clock::isDate($date)) {
                     throw new \RuntimeException('Unable to parse dates string.');
                 }
 
-                $parsed[] = Clock::fromString($date);
+                $parsed[] = Clock::fromFormat($this->dateTimeFormat, $date);
             }
         } else {
             foreach (explode($this->dateTimeStringDelimiter, $dates) as $date) {
@@ -57,21 +53,15 @@ trait Dates
                     throw new \RuntimeException('Unable to parse dates string.');
                 }
 
-                $parsed[] = Clock::fromString($date);
+                $parsed[] = Clock::fromFormat($this->dateTimeFormat, $date);
             }
         }
 
         return $parsed ?? [];
     }
 
-    /**
-     * @param array $queryParams
-     * @return Select
-     */
-    protected function buildSelectQuery(array $queryParams): Select
+    protected function datesHook(Select $select, array $queryParams): Select
     {
-        $select = parent::buildSelectQuery($queryParams);
-
         if (isset($queryParams['dates'])) {
             $dates = $this->parseDates($queryParams['dates']);
             $select = $this->dates($select, $dates);
