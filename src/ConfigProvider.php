765c826ab1a636c05997e37850d371ce016c271e
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-01-23 22:31:56 +0200

    Update ConfigProvider.php

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -6,15 +6,16 @@ use Cycle\ORM\ORMInterface;
 use Cycle\ORM\PromiseFactoryInterface;
 use Cycle\ORM\SchemaInterface;
 use Spiral\Database\DatabaseManager;
+use Cycle\ORM\EntityManager;
 
 class ConfigProvider extends \Bermuda\Config\ConfigProvider
 {
     protected function getFactories(): array
     {
         return [
+            EntityManagerInterface::class => static fn(ContainerInterface $container) => new EntityManager($container->get(ORMInterface::class));
             DatabaseManager::class => DatabaseManagerFactory::class,
             SchemaInterface::class => SchemaFactory::class,
-            PromiseFactoryInterface::class => PromiseProxyFactory::class,
             ORMInterface::class => ORMFactory::class
         ];
     }
