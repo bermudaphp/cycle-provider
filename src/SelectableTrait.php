6c570aba8217f9d5aac0929a42a69a8c74b22f8d
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2023-02-18 18:34:23 +0200

    Create SelectableTrait.php

---
+++ src/SelectableTrait.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Query;
+
+use Cycle\ORM\SchemaInterface;
+use Cycle\ORM\Select;
+
+trait SelectableTrait
+{
+    public function apply(Select $select, SchemaInterface $schema): Select
+    {
+        $callbacks = $this->getApplyCallbacks();
+        foreach ($this->queryParams as $name => $value) {
+            if (isset($callbacks[$name])) $select = $callbacks[$name]($select, $schema, $value);
+            else $select = $select->where($name, $this->queryParams[$name]);
+        }
+
+        return $this->finalize($select);
+    }
+
+    protected function finalize(Select $select): Select
+    {
+        return $select;
+    }
+
+    /**
+     * $callback(Select $select, SchemaInterface $schema, mixed $value)
+     * @return callable[]
+     */
+    protected function getApplyCallbacks(): array
+    {
+        return [];
+    }
+}
