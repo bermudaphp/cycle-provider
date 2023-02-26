8c4d93d8aff89850a8b71c26042e2d3cf2aed577
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2023-02-26 19:03:19 +0200

    Update Repository.php

--- src/Repository.php
+++ src/Repository.php
@@ -2,13 +2,10 @@
 
 namespace Bermuda\Cycle;
 
-use Bermuda\Paginator\QueryException;
-use Bermuda\Paginator\Paginator;
-use Bermuda\Paginator\QueryInterface;
+use Cycle\ORM\Select;
 use Cycle\ORM\EntityManager;
 use Cycle\ORM\EntityManagerInterface;
 use Cycle\ORM\ORMInterface;
-use Cycle\ORM\Select;
 
 abstract class Repository extends Select\Repository
 {
