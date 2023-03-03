412c6f53714e8fb3a0b1c9157146462180042587
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2023-03-03 22:30:24 +0200

    Update RowFetcher.php

--- src/RowFetcher.php
+++ src/RowFetcher.php
@@ -8,7 +8,6 @@ use Cycle\ORM\SchemaInterface;
 
 class RowFetcher
 {
-    protected array $columns;
     protected CastableInterface $handler;
 
     public function __construct(
@@ -18,18 +17,10 @@ class RowFetcher
         $handler = $orm->getSchema()->define($role, SchemaInterface::TYPECAST_HANDLER);
         $this->handler = new $handler($orm->getSource($role)->getDatabase());
         $this->handler->setRules($orm->getSchema()->define($role, SchemaInterface::TYPECAST));
-        $this->columns = array_flip($orm->getSchema()->define($role, SchemaInterface::COLUMNS));
     }
 
     public function fetch(array $row): array
     {
-        $columns = $this->columns;
-
-        foreach ($row as $column => $value) {
-            if (isset($columns[$column]) && !is_numeric($columns[$column])) $results[$columns[$column]] = $value;
-            else $results[$column] = $value;
-        }
-
-        return array_filter($this->handler->cast($results ?? []), static fn($v) => $v !== null);
+        return $this->handler->cast(array_filter($row, static fn($v) => $v !== null));
     }
 }
