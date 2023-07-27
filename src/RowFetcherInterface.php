8fc36d39260840def91f1ce74406d80c99c31c68
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2023-07-27 14:09:12 +0200

    Update RowFetcherInterface.php

--- src/RowFetcherInterface.php
+++ src/RowFetcherInterface.php
@@ -5,4 +5,5 @@ namespace Bermuda\Cycle;
 interface RowFetcherInterface
 {
     public function fetch(array $row): array ;
+    public function extend(RowFetcherInterface $fetcher): RowFetcherInterface ;
 }
